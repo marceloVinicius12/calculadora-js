87018c95feb6dc8596e69df8cbf6537746e854c3
marcelo vinicius <viniciusmarcello12@gmail.com>
2021-03-31 21:59:15 -0300

    adição das operações

--- js/funcoes.js
+++ js/funcoes.js
@@ -4,7 +4,16 @@ let operacao = parseInt(prompt("escolha 1 para soma, 2 para subtração, 3 para 
 
 
 let segundoNumero = parseFloat(prompt("informe o segundo numero"));
-
-let resultado = primeiroNumero + segundoNumero ;
-
+if(operacao == 1){
+let resultado = primeiroNumero + segundoNumero;
 document.write("<h2>" + primeiroNumero + " + " + segundoNumero + " = " + resultado + "</h2>");
+}else if(operacao == 2){
+    let resultado = primeiroNumero - segundoNumero;
+document.write("<h2>" + primeiroNumero + " - " + segundoNumero + " = " + resultado + "</h2>");
+}else if(operacao == 3){
+    let resultado = primeiroNumero * segundoNumero;
+document.write("<h2>" + primeiroNumero + " x " + segundoNumero + " = " + resultado + "</h2>");
+}else if(operacao == 4){
+    let resultado = primeiroNumero / segundoNumero;
+document.write("<h2>" + primeiroNumero + " / " + segundoNumero + " = " + resultado + "</h2>");
+}
