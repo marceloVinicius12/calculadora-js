3e0d0d9698ea40cf3218a744473b388ee943eeaa
marcelo vinicius <viniciusmarcello12@gmail.com>
2021-03-31 21:30:08 -0300

    adcionando os numeros as operções e mostrando o resultado

---
+++ js/funcoes.js
@@ -0,0 +1,10 @@
+let primeiroNumero = parseFloat(prompt("informe o primeiro número"));
+
+let operacao = parseInt(prompt("escolha 1 para soma, 2 para subtração, 3 para multiplicação e 4 para divisão"));
+
+
+let segundoNumero = parseFloat(prompt("informe o segundo numero"));
+
+let resultado = primeiroNumero + segundoNumero ;
+
+document.write("<h2>" + primeiroNumero + " + " + segundoNumero + " = " + resultado + "</h2>");
